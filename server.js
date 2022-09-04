18317a267313845718ee95b75fb922f580f4be15
Diala-Sleem <diala.slim@yahoo.com>
2022-09-04 15:42:35 +0200

    step2

--- server.js
+++ server.js
@@ -1,14 +1,26 @@
 const express = require("express");
 const app = express();
-const path=require("path");
+const path = require("path");
 //....................... static file ............................
-app.use(express.static(path.join(__dirname, 'assets')))
-app.set('view engine', 'ejs')
+app.use(express.static(path.join(__dirname, "assets")));
+app.set("view engine", "ejs");
 app.set("views", "views");
 //----------------------------------------------------------------
 
 app.get("/", (req, res, next) => {
-    res.render('index')
+  res.render("index");
+});
+
+app.get("/about", (req, res, next) => {
+  res.render("about");
+});
+
+app.get("/contact", (req, res, next) => {
+  res.render("contact");
+});
+
+app.get("/products", (req, res, next) => {
+  res.render("products");
 });
 
 //----------------------------------------------------------------
